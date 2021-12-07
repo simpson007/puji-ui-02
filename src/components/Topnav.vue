fe593ff4f278bae62a8238efc746341837e99982
鸣雷 <9728292+ming-lei-yu@user.noreply.gitee.com>
2021-12-07 20:49:48 +0800

    完善 aside 切换逻辑

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,23 +1,24 @@
 <template>
   <div class="topnav">
-    <div class="logo" @click="toggleMenu">LOGO</div>
+    <div class="logo">LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
+    <span class="toggleAside" @click="toggleMenu"></span>
   </div>
 </template>
 <script lang="ts">
-import { inject, Ref } from 'vue'
+import { inject, Ref } from "vue"
 export default {
-  setup(){
-    const menuVisible = inject<Ref<boolean>>('menuVisible') // get
-    const toggleMenu = ()=>{
-      menuVisible.value = !menuVisible.value
-    }
-    return {toggleMenu}
-  }
-}
+  setup() {
+    const menuVisible = inject<Ref<boolean>>("menuVisible");
+    const toggleMenu = () => {
+      menuVisible.value = !menuVisible.value;
+    };
+    return { toggleMenu };
+  },
+};
 </script>
 <style lang="scss" scoped>
 .topnav {
@@ -26,6 +27,8 @@ export default {
   padding: 16px;
   position: relative;
   z-index: 10;
+  justify-content: center;
+  align-items: center;
   > .logo {
     max-width: 6em;
     margin-right: auto;
@@ -36,6 +39,27 @@ export default {
     flex-wrap: nowrap;
     > li {
       margin: 0 1em;
+    }
+  }
+  > .toggleAside {
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+    display: none;
+  }
+  @media (max-width: 500px) {
+    > .menu {
+      display: none;
+    }
+    > .logo {
+      margin: 0 auto;
+    }
+    > .toggleAside {
+      display: inline-block;
     }
   }
 }
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -6,7 +6,7 @@
         <h2>组件列表</h2>
         <ol>
           <li>
-            <router-link to="/doc/swich">Switch 组件</router-link>
+            <router-link to="/doc/switch">Switch 组件</router-link>
           </li>
           <li>
             <router-link to="/doc/button">Button 组件</router-link>
@@ -26,13 +26,13 @@
 
 <script lang="ts">
 import Topnav from '../components/Topnav.vue'
-import { inject, Ref } from 'vue';
+import { inject, Ref } from "vue";
 export default {
   components: { Topnav },
-  setup(){
-    const menuVisible = inject<Ref<boolean>>('menuVisible')
-    return {menuVisible}
-  }
+  setup() {
+    const menuVisible = inject<Ref<boolean>>("menuVisible"); // get
+    return { menuVisible };
+  },
 };
 </script>
 
@@ -41,10 +41,6 @@ aside {
   background: lightblue;
   width: 150px;
   padding: 16px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  padding-top: 70px;
   > h2 {
     margin-bottom: 4px;
   }
@@ -52,6 +48,12 @@ aside {
     > li {
       padding: 4px 0;
     }
+  }
+  @media (max-width: 500px) {
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding-top: 70px;
   }
 }
 </style>
