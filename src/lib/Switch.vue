de80552fbb5574515e7bc24b148681537ffd1fd6
鸣雷 <9728292+ming-lei-yu@user.noreply.gitee.com>
2021-12-07 21:47:54 +0800

    Switch 代码优化

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,25 +1,39 @@
 <template>
-  <button> <span></span> </button>
+  <button @click="toggle" :class="{checked:value}">
+    <span></span>
+  </button>
 </template>
+<script lang="ts">
+export default {
+  props: {
+    value: Boolean,
+  },
+  setup(props, context) {
+    const toggle = () => {
+      context.emit("update:value", !props.value);
+    };
+    return { toggle };
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 $h: 22px;
 $h2: $h - 4px;
-button{
-  height: $h;
-  width: $h*2;
-  border: none;
-  background: blue;
-  border-radius: $h/2;
-  position: relative;
-}
-span{
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background:white;
-  border-radius: $h2 / 2;
+button {
+  height: $h; width: $h * 2; border: none; background: #bfbfbf; border-radius: $h/2; position: relative;
+  > span {
+    position: absolute; top: 2px; left: 2px; height: $h2; width: $h2; background: white; border-radius: $h2 / 2; transition: all 250ms;
+  }
+  &.checked { background: #1890ff;
+    > span { left: calc(100% - #{$h2} - 2px); }
+  }
+  &:focus { outline: none; }
+  &:active {
+    > span { width: $h2 + 4px; }
+  }
+  &.checked:active {
+    > span { width: $h2 + 4px; margin-left: -4px; }
+  }
 }
 </style>
