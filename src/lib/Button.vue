2c8c33c5ba0812163ae60d9253e7ee80c67fa03f
鸣雷 <9728292+ming-lei-yu@user.noreply.gitee.com>
2021-12-08 19:52:34 +0800

    支持 level 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -14,13 +14,18 @@ export default {
         type: String,
         default: "normal",
       },
+      level: {
+        type: String,
+        default: "normal",
+      },
     },
     setup(props) {
-      const { theme, size } = props;
+      const { theme, size,level } = props;
       const classes = computed(() => {
         return {
           [`puji-theme-${theme}`]: theme,
           [`puji-size-${size}`]: size,
+          [`puji-level-${level}`]: level,
         };
       });
       return { classes };
@@ -34,6 +39,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .puji-button {
   box-sizing: border-box;
   height: $h;
@@ -48,6 +54,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -88,6 +95,53 @@ $radius: 4px;
     font-size: 12px;
     height: 20px;
     padding: 0 4px;
+  }
+  &.puji-theme-button {
+    &.puji-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.puji-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.puji-theme-link {
+    &.puji-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.puji-theme-text {
+    &.puji-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.puji-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
   }
 }
 </style>
