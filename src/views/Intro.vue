cdab658aa252ae9f72720db7176f1bcb1462247a
鸣雷 <9728292+ming-lei-yu@user.noreply.gitee.com>
2021-12-11 10:47:50 +0800

    引入 github 的 markdown 样式

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    Intro
-  </div>
+  <article class="markdown-body">
+    <h1>介绍</h1>
+    <p>Puji UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
+    <p>这款组件库是我个人编写，尽量不采用第三方库。</p>
+    <p>因此不建议您将此 UI 库用于生产环境。当然这个源代码还是值得一看的。源代码放在了github 和 码云，历史提交信息非常规范，你可以按提交的顺序逐个查看；你也可以直接查看每个组件的源代码和示例，运行方法见 README.md。</p>
+    下一节：<a href="#/doc/install">安装</a>
+  </article>
 </template>
