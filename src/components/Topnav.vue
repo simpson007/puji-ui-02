ed3bbabef16dd157b3930a76ca00d782528ce75f
鸣雷 <9728292+ming-lei-yu@user.noreply.gitee.com>
2021-12-11 10:26:44 +0800

    高亮路由

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,16 +1,32 @@
 <template>
   <div class="topnav">
-    <div class="logo">LOGO</div>
+    <router-link to="/" class="logo">
+      <svg class="icon">
+        <use xlink:href="#icon-puji"></use>
+      </svg>
+    </router-link>
     <ul class="menu">
-      <li>菜单1</li>
-      <li>菜单2</li>
+      <li>
+        <router-link to="/doc">文档</router-link>
+      </li>
     </ul>
-    <span class="toggleAside" @click="toggleMenu"></span>
+    <svg v-if="toggleMenuButtonVisible" class="toggleAside" @click="toggleMenu">
+      <use xlink:href="#icon-menu"></use>
+    </svg>
   </div>
 </template>
 <script lang="ts">
-import { inject, Ref } from "vue"
+import {
+  inject,
+  Ref
+} from "vue";
 export default {
+  props: {
+    toggleMenuButtonVisible: {
+      type: Boolean,
+      default: false
+    }
+  },
   setup() {
     const menuVisible = inject<Ref<boolean>>("menuVisible");
     const toggleMenu = () => {
@@ -21,8 +37,8 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+$color: #007974;
 .topnav {
-  background: pink;
   display: flex;
   padding: 16px;
   position: fixed;
@@ -35,6 +51,10 @@ export default {
   > .logo {
     max-width: 6em;
     margin-right: auto;
+    >svg {
+      width: 32px;
+      height: 32px;
+    }
   }
   > .menu {
     display: flex;
@@ -45,14 +65,15 @@ export default {
     }
   }
   > .toggleAside {
-    width: 24px;
-    height: 24px;
+    width: 32px;
+    height: 32px;
     background: red;
     position: absolute;
     left: 16px;
     top: 50%;
     transform: translateY(-50%);
     display: none;
+    background: fade-out(black, 0.9);
   }
   @media (max-width: 500px) {
     > .menu {
